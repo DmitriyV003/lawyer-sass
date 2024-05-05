bd72444bbfa90522ee5b845e863ecca4a268e9a2
DmitriyV003 <vdmitriy003@gmail.com>
2024-05-05 18:34:19 +0300

    feat: ResponseServiceProvider

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -3,28 +3,13 @@
 if (! function_exists('api_response')) {
     function api_response($content = '', $status = 200, array $headers = [])
     {
-        return response()->json(
-            [
-                'success' => 1,
-                'error' => [],
-                'data' => $content,
-            ],
-            $status,
-            $headers,
-        );
+        return response()->api($content, $status, $headers);
     }
 }
 
 if (! function_exists('api_error')) {
     function api_error($content = '', $status = 400, array $headers = [])
     {
-        return response()->json(
-            [
-                'success' => 0,
-                'data' => $content,
-            ],
-            $status,
-            $headers,
-        );
+        return response()->api_error($content, $status, $headers);
     }
 }
