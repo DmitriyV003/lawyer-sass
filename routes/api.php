f5df9565d59a43f3232470eaf7d3234bfed9274f
DmitriyV003 <vdmitriy003@gmail.com>
2024-05-15 19:29:40 +0300

    feat: admin application

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\AuthController as AdminAuthControllerAlias;
 use App\Http\Controllers\Admin\RegistrationController as AdminRegistrationControllerAlias;
 use App\Http\Controllers\Admin\TariffController as AdminTariffController;
 use App\Http\Controllers\ApplicationController;
+use App\Http\Controllers\Admin\ApplicationController as AdminApplicationController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AuthorityController;
 use App\Http\Controllers\LawsuitCategoryController;
@@ -32,50 +33,53 @@ Route::prefix('/v1')->group(function () {
     });
 
     Route::middleware('auth:api')->group(function () {
-        Route::prefix('/admin')->group(function () {
+        Route::middleware('role:super admin')->prefix('/admin')->group(function () {
+            Route::get('/application', AdminApplicationController::class);
             Route::patch('/tariff/{tariff}/update-status', [AdminTariffController::class, 'updateStatus']);
             Route::apiResource('tariff', AdminTariffController::class)
                 ->only('index', 'show', 'store', 'destroy', 'update')
                 ->withTrashed();
-        })->middleware('role:super admin');
+        });
 
-        Route::get('/application', ApplicationController::class);
-        Route::prefix('/auth')->group(function () {
-            Route::post('/logout', [AuthController::class, 'logout']);
+        Route::middleware('role:advocate')->group(function () {
+            Route::get('/application', ApplicationController::class);
+            Route::prefix('/auth')->group(function () {
+                Route::post('/logout', [AuthController::class, 'logout']);
+            });
+            Route::prefix('/user')->group(function () {
+                Route::get('/', [UserController::class, 'user']);
+                Route::delete('/', [UserController::class, 'destroy']);
+                Route::put('/', [UserController::class, 'update']);
+                Route::put('/update-password', [UserController::class, 'updatePassword']);
+            });
+            Route::apiResource('customer', CustomerController::class)
+                ->only('index', 'show', 'store', 'destroy', 'update');
+            Route::get('/customer/{customer}/events', [CustomerController::class, 'events']);
+            Route::apiResource('lawsuit-category', LawsuitCategoryController::class)
+                ->only('index', 'show', 'store', 'destroy', 'update');
+            Route::apiResource('lawsuit', LawsuitController::class)
+                ->only('index', 'show', 'store', 'destroy', 'update');
+            Route::get('/lawsuit/{lawsuit}/authorities', [LawsuitController::class, 'authorities']);
+            Route::apiResource('authority', AuthorityController::class)
+                ->only('show', 'store', 'destroy', 'update');
+            Route::apiResource('task-tag', TaskTagController::class)
+                ->only('index', 'show', 'store', 'destroy', 'update');
+            Route::apiResource('lawsuit-event-category', LawsuitEventCategoryController::class)
+                ->only('index', 'show', 'store', 'destroy', 'update');
+            Route::prefix('/lawsuit-event')->group(function () {
+                Route::post('/{lawsuitEvent}/status', [LawsuitEventController::class, 'status']);
+                Route::get('/group', [LawsuitEventController::class, 'groupEvents']);
+            });
+            Route::apiResource('lawsuit-event', LawsuitEventController::class)
+                ->only('index', 'show', 'store', 'destroy', 'update');
+            Route::apiResource('/note', NoteController::class)
+                ->only('index', 'show', 'store', 'destroy', 'update');
+            Route::prefix('/task')->group(function () {
+                Route::patch('/{task}/to-do-date', [TaskController::class, 'updateToDoDate']);
+                Route::post('/{task}/status', [TaskController::class, 'status']);
+            });
+            Route::apiResource('task', TaskController::class)
+                ->only('index', 'show', 'store', 'destroy', 'update');
         });
-        Route::prefix('/user')->group(function () {
-            Route::get('/', [UserController::class, 'user']);
-            Route::delete('/', [UserController::class, 'destroy']);
-            Route::put('/', [UserController::class, 'update']);
-            Route::put('/update-password', [UserController::class, 'updatePassword']);
-        });
-        Route::apiResource('customer', CustomerController::class)
-            ->only('index', 'show', 'store', 'destroy', 'update');
-        Route::get('/customer/{customer}/events', [CustomerController::class, 'events']);
-        Route::apiResource('lawsuit-category', LawsuitCategoryController::class)
-            ->only('index', 'show', 'store', 'destroy', 'update');
-        Route::apiResource('lawsuit', LawsuitController::class)
-            ->only('index', 'show', 'store', 'destroy', 'update');
-        Route::get('/lawsuit/{lawsuit}/authorities', [LawsuitController::class, 'authorities']);
-        Route::apiResource('authority', AuthorityController::class)
-            ->only('show', 'store', 'destroy', 'update');
-        Route::apiResource('task-tag', TaskTagController::class)
-            ->only('index', 'show', 'store', 'destroy', 'update');
-        Route::apiResource('lawsuit-event-category', LawsuitEventCategoryController::class)
-            ->only('index', 'show', 'store', 'destroy', 'update');
-        Route::prefix('/lawsuit-event')->group(function () {
-            Route::post('/{lawsuitEvent}/status', [LawsuitEventController::class, 'status']);
-            Route::get('/group', [LawsuitEventController::class, 'groupEvents']);
-        });
-        Route::apiResource('lawsuit-event', LawsuitEventController::class)
-            ->only('index', 'show', 'store', 'destroy', 'update');
-        Route::apiResource('/note', NoteController::class)
-            ->only('index', 'show', 'store', 'destroy', 'update');
-        Route::prefix('/task')->group(function () {
-            Route::patch('/{task}/to-do-date', [TaskController::class, 'updateToDoDate']);
-            Route::post('/{task}/status', [TaskController::class, 'status']);
-        });
-        Route::apiResource('task', TaskController::class)
-            ->only('index', 'show', 'store', 'destroy', 'update');
     });
 });
